a331f9299e8bcd664b35fb5222f7d532538a1baf
tony <chcjswo@gmail.com>
2022-03-15 00:20:34 +0900

    unchecked exception으로 수정

--- src/main/java/com/mocadev/cleancode/policy/BrokeragePolicyFactory.java
+++ src/main/java/com/mocadev/cleancode/policy/BrokeragePolicyFactory.java
@@ -1,6 +1,8 @@
 package com.mocadev.cleancode.policy;
 
 import com.mocadev.cleancode.contants.ActionType;
+import com.mocadev.cleancode.exception.ErrorCode;
+import com.mocadev.cleancode.exception.HouseUtilsException;
 
 /**
  * @author chcjswo
@@ -18,7 +20,7 @@ public class BrokeragePolicyFactory {
 			case PURCHASE:
 				return new PurchaseBrokeragePolicy();
 			default:
-				throw new IllegalArgumentException("해당 actionType에 대한 정책이 존재하지 않습니다.");
+				throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 actionType에 대한 정책이 존재하지 않습니다.");
 		}
 	}
 }
